e67d95ce2e29cbf1dee0cbcf4ac0a9f7f118fe09
tuftman-0 <joshmartin1010@gmail.com>
2025-01-19 20:02:23 -0500

    removed ghostty from flake

--- flake.nix
+++ flake.nix
@@ -5,27 +5,34 @@
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
     stable.url = "github:nixos/nixpkgs?ref=nixos-23.11";
     ki-editor.url = "github:ki-editor/ki-editor";
-    ghostty = {
-      url = "git+ssh://git@github.com/ghostty-org/ghostty";
-      inputs.nixpkgs-stable.follows = "nixpkgs";
-      inputs.nixpkgs-unstable.follows = "nixpkgs";
-    };
+    # ghostty = {
+    #   url = "git+ssh://git@github.com/ghostty-org/ghostty";
+    #   inputs.nixpkgs-stable.follows = "nixpkgs";
+    #   inputs.nixpkgs-unstable.follows = "nixpkgs";
+    # };
   };
 
-  outputs = { self, nixpkgs, stable, ki-editor, ghostty, ...  }: {
+  outputs = {
+    self,
+    nixpkgs,
+    stable,
+    ki-editor,
+    # ghostty,
+    ...
+  }: {
     nixosConfigurations."bapanada" = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
-        modules = [
-          ({ pkgs, ... } : {system.configurationRevision = nixpkgs.lib.mkIf (self ? rev) self.rev;})
-          {
-            environment.systemPackages = [
-              ki-editor.packages.x86_64-linux.default
-              stable.legacyPackages.x86_64-linux.bibata-cursors
-              ghostty.packages.x86_64-linux.default
-            ];
-          }
-          ./configuration.nix
-          ./nvidia.nix
+      modules = [
+        ({pkgs, ...}: {system.configurationRevision = nixpkgs.lib.mkIf (self ? rev) self.rev;})
+        {
+          environment.systemPackages = [
+            ki-editor.packages.x86_64-linux.default
+            stable.legacyPackages.x86_64-linux.bibata-cursors
+            # ghostty.packages.x86_64-linux.default
+          ];
+        }
+        ./configuration.nix
+        ./nvidia.nix
       ];
     };
   };
