a4bbcbe9f9a6763d33ffe38962b10db246f032e4
tuftman-0 <28765011+tuftman-0@users.noreply.github.com>
2025-07-16 12:26:45 -0400

    removed ki

--- flake.nix
+++ flake.nix
@@ -26,7 +26,7 @@
         ({pkgs, ...}: {system.configurationRevision = nixpkgs.lib.mkIf (self ? rev) self.rev;})
         {
           environment.systemPackages = [
-            ki-editor.packages.x86_64-linux.default
+            # ki-editor.packages.x86_64-linux.default
             stable.legacyPackages.x86_64-linux.bibata-cursors
             stable.legacyPackages.x86_64-linux.jetbrains.idea-community
             # ghostty.packages.x86_64-linux.default
