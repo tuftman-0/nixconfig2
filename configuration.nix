ed5364df4d8129dceebfc15bcf76da54eca17b9d
tuftman-0 <28765011+tuftman-0@users.noreply.github.com>
2025-12-23 14:22:50 -0500

    turned off file roller service because it's no longer needed apparently maybe add program

--- configuration.nix
+++ configuration.nix
@@ -176,7 +176,7 @@ in
     plugins = with pkgs.xfce; [thunar-archive-plugin thunar-volman];
   };
 
-  programs.file-roller.enable = true;
+  # programs.file-roller.enable = true;
   services.gvfs.enable = true; # Mount, trash, and other functionalities
   services.tumbler.enable = true; # Thumbnail support for images
   
